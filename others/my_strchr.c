48d30556399238fb65cc51d9b10c1164e8ff7197
Elmir_Musaev <no_email@secret>
2023-04-13 17:34:08 +0400

    my_strchr rewritten

--- others/my_strchr.c
+++ others/my_strchr.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
-char* my_strchr(char* a, int c);
+char *my_strchr(char *str, int c) {
+
+  char *ret = NULL;
+
+  while (*str != '\0') {
+    if (*str == (char)c) {
+      ret = (char *)str;
+      break;
+    }
+    *str++;
+  }
+
+  return ret;
+}
 
 int main() {
 
-    char *a = "Hello";
-    char *b = my_strchr(a, 'l');
-
-    return 0;
-}
-
-char* my_strchr(char* a, int c) {
-
-    int count = 0;
-    int flag = 0;
-
-    while (a[count] != '\0') {
-        if (a[count] == (char)c) {
-            while (a[count] != '\0') {
-                printf("%c", a[count]);
-                count++;
-            }
-            flag = 1;
-            break;
-        }
-        count++;
-    }
-    if (flag == 1)
-        printf("\n");
-
-    return a;
+  char *str = "Hello";
+  char *res = my_strchr(str, 'l');
+  printf("%s\n", res);
+  return 0;
 }

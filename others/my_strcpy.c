94d4fce9ff0066852736dc256bf22f95e5b86f07
Elmir_Musaev <no_email@secret>
2023-04-15 15:08:39 +0400

    strcpy fixed

--- others/my_strcpy.c
+++ others/my_strcpy.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* my_strcpy(char* a, char* b);
+char *my_strcpy(char *src, char *str) {
+
+  int i = 0;
+  while (str[i] != '\0') {
+    src[i] = str[i];
+    i++;
+  }
+  src[i] = '\0';
+
+  return src;
+}
+
 
 int main() {
 
-    char* a = "Hello";
-    char* b = "World!";
-    /*char* c = */printf("%s\n", my_strcpy(a,b));
-    //printf("%s\n", c);
-
-    return 0;
-}
-
-char* my_strcpy(char* a, char* b) {
-    char *c = malloc(1*sizeof(char));
-    int count = 0;
-    int i = 0;
-    while (a[count] != '\0') {
-        c[count] = a[count];
-        count++;
-    }
-    while (b[i] != '\0') {
-        c[count] = b[i];
-        count++;
-        i++;
-    }
-    return c;
+  char str[100];
+  printf("%s\n", my_strcpy(str, "Succesfully copied."));
+  printf("%s\n", str);
+  return 0;
 }
